7a2e1284df99f61df92c792ebebadd852e1124e3
Karen <karenwellsfreeman@gmail.com>
2024-06-05 10:32:09 -0300

    commit

---
+++ my-blog/src/pages/ArticlePage.js
@@ -0,0 +1,7 @@
+const ArticlePage = () => {
+    return (
+
+    <h1>This is the Home Page</h1>
+    )
+}
+export default ArticlePage;
---
+++ my-blog/src/pages/NotFoundPage.js
@@ -0,0 +1,7 @@
+const NotFoundPage = () => {
+    return (
+
+    <h1>This is the Home Page</h1>
+    )
+}
+export default NotFoundPage;
