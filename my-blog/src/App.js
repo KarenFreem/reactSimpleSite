5a9a2991f8bfeb39f9f5e8a5cc57c881685278ac
Karen <karenwellsfreeman@gmail.com>
2024-06-05 21:19:16 -0300

    NavBar

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
 import AboutPage from './pages/AboutPage';
 import ArticleListPage from './pages/ArticleListPage';
 import ArticlePage from './pages/ArticlePage';
@@ -11,7 +12,7 @@ function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      <h1>Karen's Blog</h1>
+      <NavBar />
       <div id="page-body">
           <Routes>
             <Route path="/" element={<HomePage />}/>
